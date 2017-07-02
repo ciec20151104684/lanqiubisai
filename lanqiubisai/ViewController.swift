8ece85035fa39a7002f3cac7d0ee98d711f97ec9
ciec20151104684 <s20151104684@King.local>
2017-07-02 09:05:31 +0800

    上传图片
    完成上传图片功能，无连接数据库

--- lanqiubisai/ViewController.swift
+++ lanqiubisai/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+    UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
 
     @IBOutlet weak var text1: UILabel!
     
@@ -21,6 +23,98 @@ class ViewController: UIViewController {
     var b:Int = 0
     var c:Int = 0
     var d:Int = 0
+    
+    
+    
+    var dlg:Int = 0;
+    var flagA:Int = 0;
+    var flagB:Int = 0;
+    
+    @IBOutlet weak var phtotoone: UIImageView!
+    
+    
+    
+    @IBOutlet weak var phototwo: UIImageView!
+    
+    
+    @IBAction func photo1(_ sender: Any) {
+        if(dlg==0){
+            flagA=1
+            flagB=0
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                //初始化图片控制器
+                let picker = UIImagePickerController()
+                //设置代理
+                picker.delegate = self
+                //指定图片控制器类型
+                picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+                //弹出控制器，显示界面
+                self.present(picker,animated: true,
+                             completion: {
+                                ()->Void in
+                })
+                
+            }else{
+                print("读取相册错误")
+            }
+        }
+        
+    
+    }
+    
+    
+    
+    
+    @IBOutlet weak var photo1: UIButton!
+    
+    @IBAction func photo2(_ sender: Any) {
+        if(dlg==0){
+            flagA=0
+            flagB=1
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                //初始化图片控制器
+                let picker = UIImagePickerController()
+                //设置代理
+                picker.delegate = self
+                //指定图片控制器类型
+                picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+                //弹出控制器，显示界面
+                self.present(picker,animated: true,
+                             completion: {
+                                ()->Void in
+                })
+                
+            }else{
+                print("读取相册错误")
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //查看inof对象
+        print(info)
+        //显示的图片
+        let image:UIImage!
+        //获取选择原因
+        image=info[UIImagePickerControllerOriginalImage] as! UIImage
+        if(flagA==1){
+            phtotoone.image = image
+        }
+        else if(flagB==1){
+            phototwo.image = image
+        }
+        //图片控制器退出
+        picker.dismiss(animated: true, completion: {
+            () -> Void in
+        })
+    }
+    
+    
+    
+    
+    
+    
+    
     @IBAction func leftjia(_ sender: Any) {
        a=a+1
         text1.text=("\(a)");
@@ -35,7 +129,7 @@ class ViewController: UIViewController {
         }
         
         if(c==3){
-            let alertVC = UIAlertController(title: "提示", message: "张继科胜！！！", preferredStyle: UIAlertControllerStyle.alert)
+            let alertVC = UIAlertController(title: "比赛结果", message: "张继科胜！！！", preferredStyle: UIAlertControllerStyle.alert)
             c=0;
             d=0;
             text3.text=("\(c)");
@@ -75,7 +169,7 @@ class ViewController: UIViewController {
         }
         
         if(d==3){
-            let alertVC = UIAlertController(title: "提示", message: "马龙胜！！！", preferredStyle: UIAlertControllerStyle.alert)
+            let alertVC = UIAlertController(title: "比赛结果", message: "马龙胜！！！", preferredStyle: UIAlertControllerStyle.alert)
             
             c=0;
             d=0;
@@ -89,7 +183,7 @@ class ViewController: UIViewController {
             }
             alertVC.addAction(acSure)
             alertVC.addAction(acCancel)
-            self.present(alertVC, animated: true, completion: nil)
+            self.present(alertVC, animated: true, completion:   nil)
             
             
         }
